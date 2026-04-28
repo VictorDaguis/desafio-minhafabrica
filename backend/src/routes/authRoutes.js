8d2dec9fd4629e29d039fdb81f8fe862388a5e00
VictorDaguis <vthugo3321@gmail.com>
2026-04-28 06:09:14 -0300

    Comentários
    Added a comment to explain route definitions and middleware usage.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-
+//Define quais URLs existem e quem responde cada uma, Por exemplo: POST /api/v1/auth/login chama o authController.login. Também aplica o middleware de autenticação nas rotas protegidas.
 const router = express.Router();
 
 router.post("/login", authController.login);
